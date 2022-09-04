e076425ebea18771ab78ff90515d9d8212110bda
gustavoretamero <102263843+gustavoretamero@users.noreply.github.com>
2022-09-04 20:29:12 -0300

    Add files via upload

---
+++ exercicio 1.java
@@ -0,0 +1,57 @@
+public class Invoice {
+	String num;
+	String desc;
+	int quant;
+	double preco;
+	
+	public Invoice(int iniciaQuant,double valor){
+		
+		if( iniciaQuant > 0){
+			quant = iniciaQuant;
+		} else {
+			quant = 0;
+		}
+		
+		if(valor > 0.0){
+			preco  = valor;
+		} else {
+			preco = 0.0;
+		}
+		
+	}
+	
+	public void setQuant(int quantid){
+		quant = quantid;
+	}
+	
+	public void setNum(String numero){
+		num = numero;
+	}
+	public void setDesc(String descricao){
+		desc = descricao;
+	}
+	public void setPreco(Double valor){
+		preco = valor;
+	}
+	
+	public int getQuant(){
+		return quant;
+	}
+	public String getNum(){
+		return num;
+	}
+	public String getDescricao(){
+		return desc;
+	}
+	public double getPreco(){
+		return preco;
+	}
+	
+	public double GetInvoiceAmount(){
+		
+		double total = quant * preco;
+		
+		return total;
+		
+	}
+}
